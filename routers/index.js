6ff374abbbdbdfadb31ccfcc11998f96b82f1427
zuarxanna <nikhsan417@gmail.com>
2021-12-08 12:04:14 +0700

    refactoring nodemailer

--- routers/index.js
+++ routers/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 // const sendMail = require('../utils/mail')
 const nodemailer = require('nodemailer');
-const mailgun = require('nodemailer-mailgun-transport');
+const smtpTransport = require('nodemailer-smtp-transport')
+
 // root
 router.get('/', (req, res) => {
     res.render('index');
@@ -9,24 +10,46 @@ router.get('/', (req, res) => {
 
 // contact
 router.post('/contact', (req, res) => {
-    const auth = {
+    const { name, email, subject, text } = req.body;
+    const output = `
+        <p> You Have New Message</p>
+        <h3> Contact Details </h3>
+        <ul>
+            <li>
+                Name: ${name}
+            </li>
+            <li>
+                Email: ${email}
+            </li>
+            <li>
+                Subject: ${subject}
+            </li>
+        </ul>
+        <h3>Message</h3>
+        <p>${text}</p>
+    `
+    const to = 'nikhsan417@gmail.com';
+    var transporter = nodemailer.createTransport(smtpTransport({
+        service: 'gmail',
+        host: 'smtp.gmail.com',
         auth: {
-            api_key: 'e68994af7ab3ef8951228e9731f3ecec-7005f37e-83c3eb28',
-            domain: 'sandbox9176b6a6a141463da3ae141181485af5.mailgun.org'
-            
+          user: process.env.EMAIL,
+          pass: process.env.PASS
         }
+      }));
+    var mailOptions = {
+        from: email,
+        to: to, 
+        subject: `${name} | ${subject}`,
+        html: output
     }
-    const transporter = nodemailer.createTransport(mailgun(auth));
-
-    const mailOptions = {
-        from: req.body.email,
-        to: 'nikhsan417@gmail.com',
-        subject: req.body.subject,
-        text: `Hi, I'am ${req.body.name}, ${req.body.text}`
-    };
-
-    transporter.sendMail(mailOptions, (err, data) => {
-        err ? console.log(err) : res.redirect('/');
-    })
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+    });   
+    res.redirect('/')
 })
 module.exports = router;
