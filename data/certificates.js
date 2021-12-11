5d80bfc696cd5fe9af731f7e7329f8dda0e913e5
zuarxanna <nikhsan417@gmail.com>
2021-12-11 21:22:08 +0700

    update

--- data/certificates.js
+++ data/certificates.js
@@ -4,7 +4,7 @@ const certificates = [
         key: 'dts-kominfo',
         icon: 'bi-mortarboard-fill',
         title: 'Kominfo Digitalent Scholarship',
-        subtitle: 'html, css, javascript',
+        subtitle: 'HTML, CSS, Javascript',
         desc: 'Selected to participate in and complete HTML, CSS, Javascript training from the Ministry of Communication and Information in the Digitalent Scholarship program.',
         link: '/assets/pdf/dts.pdf',
     }, {
@@ -12,7 +12,7 @@ const certificates = [
         key: 'binar-fsw',
         icon: 'bi-mortarboard-fill',
         title: 'Binar Academy',
-        subtitle: 'Fullstack Web Bootcamp',
+        subtitle: 'Full-stack Web Bootcamp',
         desc: 'Graduated from Binar Academy Full-Stack Web Bootcamp. Passed 11 chapter of challange, and success build an Web Based Application',
         link: '/assets/pdf/binar.pdf',
     }, {
@@ -20,8 +20,8 @@ const certificates = [
         key: 'fsw-best',
         icon: 'bi-award-fill',
         title: 'Binar Academy',
-        subtitle: 'Best Student at Full-Stack Web Bootcamp',
-        desc: 'Graduated from Binar Academy Full-Stack Web Bootacamp as The Best Student. Become the most active and most developed student.',
+        subtitle: 'Best Student at Full-stack Web Bootcamp',
+        desc: 'Graduated from Binar Academy Full-stack Web Bootcamp as The Best Student. Become the most active and most developed student.',
         link: '/assets/pdf/best.pdf',
     }
 ]
