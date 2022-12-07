035a92d59a36d650eb0e7283886873ff19781018
Muhammad Ikhsan <nikhsan417@gmail.com>
2022-12-07 13:15:20 +0700

    update

--- routers/index.js
+++ routers/index.js
@@ -1,26 +1,32 @@
-const router = require('express').Router();
+const router = require("express").Router();
 // const sendMail = require('../utils/mail')
-const nodemailer = require('nodemailer');
-const smtpTransport = require('nodemailer-smtp-transport');
-const { certificates, portfolio, testimonials, resume, about, socmed }= require('../data')
+const nodemailer = require("nodemailer");
+const smtpTransport = require("nodemailer-smtp-transport");
+const {
+  certificates,
+  portfolio,
+  testimonials,
+  resume,
+  about,
+  socmed,
+} = require("../data");
 
 // root
-router.get('/', (req, res) => {
-    console.log(socmed)
-    res.render('index', {
-        certificates,
-        portfolio,
-        testimonials,
-        resume,
-        about,
-        socmed
-    });
+router.get("/", (req, res) => {
+  res.render("index", {
+    certificates,
+    portfolio,
+    testimonials,
+    resume,
+    about,
+    socmed,
+  });
 });
 
 // contact
-router.post('/contact', (req, res) => {
-    const { name, email, subject, text } = req.body;
-    const output = `
+router.post("/contact", (req, res) => {
+  const { name, email, subject, text } = req.body;
+  const output = `
         <p> You Have New Message</p>
         <h3> Contact Details </h3>
         <ul>
@@ -36,29 +42,30 @@ router.post('/contact', (req, res) => {
         </ul>
         <h3>Message</h3>
         <p>${text}</p>
-    `
-    const to = 'nikhsan417@gmail.com';
-    var transporter = nodemailer.createTransport(smtpTransport({
-        service: 'gmail',
-        host: 'smtp.gmail.com',
-        auth: {
-          user: process.env.EMAIL,
-          pass: process.env.PASS
-        }
-      }));
-    var mailOptions = {
-        from: email,
-        to: to, 
-        subject: `${name} | ${subject}`,
-        html: output
+    `;
+  const to = "nikhsan417@gmail.com";
+  const transporter = nodemailer.createTransport(
+    smtpTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASS,
+      },
+    })
+  );
+  const mailOptions = {
+    from: email,
+    to,
+    subject: `PORTFOLIO CONTACT | ${subject}`,
+    html: output,
+  };
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
     }
-    transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-        }
-    });   
-    res.redirect('/')
-})
+  });
+  res.redirect("/");
+});
 module.exports = router;
--- utils/mail.js
+++ utils/mail.js
@@ -1,24 +1,22 @@
-const nodemailer = require('nodemailer');
-const mailgun = require('nodemailer-mailgun-transport');
+const nodemailer = require("nodemailer");
+const mailgun = require("nodemailer-mailgun-transport");
 
 const auth = {
-    auth: {
-        api_key: 'e68994af7ab3ef8951228e9731f3ecec-7005f37e-83c3eb28',
-        domain: 'sandbox9176b6a6a141463da3ae141181485af5.mailgun.org'
-        
-    }
-}
+  auth: {
+    api_key: "e68994af7ab3ef8951228e9731f3ecec-7005f37e-83c3eb28",
+    domain: "sandbox9176b6a6a141463da3ae141181485af5.mailgun.org",
+  },
+};
 
 const transporter = nodemailer.createTransport(mailgun(auth));
 
 const mailOptions = {
-    from: 'firdaus.husna@gmail.com',
-    to: 'nikhsan417@gmail.com',
-    subject: 'testing',
-    text: 'is it works ?'
+  to: "nikhsan417@gmail.com",
+  subject: "testing",
+  text: "is it works ?",
 };
 
 transporter.sendMail(mailOptions, (err, data) => {
-    if(err) return console.log(err);
-    console.log('Message Sent')
+  if (err) return console.log(err);
+  console.log("Message Sent");
 });
